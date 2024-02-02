92ee64d2257f2151ec6afd7da0999dbd3f4fe197
Agus <agusprastia1@gmail.com>
2024-02-02 21:11:21 +0700

    Convert Program.cs to Top-Level statement

--- Smartdrive/Program.cs
+++ Smartdrive/Program.cs
@@ -7,46 +7,40 @@ using Smartdrive.Repositories.Payment;
 using Smartdrive.Services.Master;
 using Smartdrive.Services.Payment;
 
-internal class Program
+var builder = WebApplication.CreateBuilder(args);
+IConfigurationRoot Configuration;
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<IRepository<CarBrand>, CarBrandRepository>();
+builder.Services.AddScoped<ICarBrandService, CarBrandService>();
+
+builder.Services.AddScoped<IRepository<UserAccount>, UserAccountRepository>();
+builder.Services.AddScoped<IUserAccountService, UserAccountsService>();
+
+
+builder.Services.AddDbContext<SmartdriveContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("SmartDriveDB")));
+//builder.Services.AddDbContext<SmartdriveContext>(
+//options => options.UseSqlServer("Server=DESKTOP-D6289E0\\SQLEXPRESS; Database = SmartDrive; Trusted_Connection=True; TrustServerCertificate=True;"));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
 {
-    private static void Main(string[] args)
-    {
-        var builder = WebApplication.CreateBuilder(args);
-        IConfigurationRoot Configuration;
-
-        // Add services to the container.
-
-        builder.Services.AddControllers();
-        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-        builder.Services.AddEndpointsApiExplorer();
-        builder.Services.AddSwaggerGen();
-
-        builder.Services.AddScoped<IRepository<CarBrand>, CarBrandRepository>();
-        builder.Services.AddScoped<ICarBrandService, CarBrandService>();
-
-        builder.Services.AddScoped<IRepository<UserAccount>, UserAccountRepository>();
-        builder.Services.AddScoped<IUserAccountService, UserAccountsService>();
-
-
-        builder.Services.AddDbContext<SmartdriveContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("SmartDriveDB")));
-        //builder.Services.AddDbContext<SmartdriveContext>(
-        //options => options.UseSqlServer("Server=DESKTOP-D6289E0\\SQLEXPRESS; Database = SmartDrive; Trusted_Connection=True; TrustServerCertificate=True;"));
-
-        var app = builder.Build();
-
-        // Configure the HTTP request pipeline.
-        if (app.Environment.IsDevelopment())
-        {
-            app.UseSwagger();
-            app.UseSwaggerUI();
-        }
-
-        app.UseHttpsRedirection();
-
-        app.UseAuthorization();
-
-        app.MapControllers();
-
-        app.Run();
-    }
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
